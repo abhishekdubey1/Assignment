b030fa06ef8bf57f1d5e2cdc37a432cf4986545b
Abhishek Dubey <ad99526@gmail.com>
2022-01-26 09:37:38 +0530

    Add error handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import produce from "immer";
 import { useEffect, useReducer, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import Followers from "./Components/Followers";
 import RepoList from "./Components/RepoList";
 import SingleRepo from "./Components/SingleRepo";
 import "./styles.css";
+
 export const getReposApi = (username = "") =>
   `https://api.github.com/users/${username}/repos`;
 
 export const pages = ["main", "single-repo", "followers"];
+
+const initialState = {
+  username: "",
+  repos: [],
+  page: pages[0],
+  repoIdx: 0,
+  error: "",
+  pageNum: 1
+};
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
@@ -27,24 +38,23 @@ const reducer = (state, { type, payload }) => {
       return produce(state, (draft) => {
         draft.repoIdx = payload;
       });
+    case "SET_ERROR":
+      return produce(state, (draft) => {
+        draft.error = payload;
+      });
+    case "RESET_STATE":
+      return initialState;
     default:
       return state;
   }
 };
 
 export default function App() {
-  // const [username, setUsername] = useState("");
-  // const [repos, setRepos] = useState([]);
-  // const [page, setPage] = useState(pages[0]);
-  // const [repoIdx, setRepoIdx] = useState(0);
-  const [{ username, repos, page, repoIdx }, dispatch] = useReducer(reducer, {
-    username: "",
-    repos: [],
-    page: pages[0],
-    repoIdx: 0,
-    error: "",
-    pageNum: 1
-  });
+  const [
+    { username, repos, page, repoIdx, error, pageNum },
+    dispatch
+  ] = useReducer(reducer, initialState);
+
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
@@ -55,36 +65,52 @@ export default function App() {
         dispatch({ type: "SET_REPOS", payload: data });
       } else if (String(response.status).startsWith("4")) {
         //throw Error
+        throw new Error("Some problem occured");
       }
     } catch (error) {
       console.log(error.message);
+      dispatch({ type: "SET_ERROR", payload: error.message });
       //show error
     }
   };
 
   return (
     <div className="App">
-      {console.log(page)}
-      {page === pages[0] && (
-        <>
-          <form onSubmit={handleSubmit}>
-            <input
-              value={username}
-              onChange={(e) =>
-                dispatch({ type: "SET_USERNAME", payload: e.target.value })
-              }
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <RepoList repos={repos} dispatch={dispatch} />
-        </>
-      )}
-      {page === pages[1] && (
-        <SingleRepo repo={repos[repoIdx]} dispatch={dispatch} />
-      )}
-      {page === pages[2] && (
-        <Followers username={username} dispatch={dispatch} />
-      )}
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => dispatch({ type: "RESET_state" })}
+      >
+        {page === pages[0] && (
+          <>
+            <form onSubmit={handleSubmit}>
+              <input
+                value={username}
+                onChange={(e) =>
+                  dispatch({ type: "SET_USERNAME", payload: e.target.value })
+                }
+              />
+              <button type="submit">Submit</button>
+            </form>
+            <RepoList repos={repos} dispatch={dispatch} />
+          </>
+        )}
+        {page === pages[1] && (
+          <SingleRepo repo={repos[repoIdx]} dispatch={dispatch} />
+        )}
+        {page === pages[2] && (
+          <Followers username={username} dispatch={dispatch} />
+        )}
+        {error !== "" && <ErrorFallback error={error} />}
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p className="error-msg">Something went wrong:</p>
+      <pre>{error.message}</pre>
     </div>
   );
 }
